d8b937a3703bcb409c834eb021a4e04d1f360887
Jeremy Greer <jeremy.greer.atl@gmail.com>
2016-12-26 22:25:16 -0600

    remove xml junk, comments, styles

--- processed/bunk.js
+++ processed/bunk.js
@@ -1,1 +1,1 @@
-asdf
+not real
--- src/fixer.js
+++ src/fixer.js
@@ -1,8 +1,53 @@
+import cheerio from 'cheerio'
+
+
+const removeComments = ($) => {
+
+  console.log($('svg').html())
+
+  /*
+  $.contents().filter(function () {
+    return this.nodeType === 8
+  }).remove()
+  */
+  return $
+}
+
+const removeXmlAndComments = ($) => {
+  $.root().contents().filter(function () {
+    return this.type !== 'tag'
+  }).remove()
+  return $
+}
+
+const removeStyles = ($) => {
+  $('style').remove()
+  return $
+}
+
+
+
+const removeCrap = ($) => {
+  $ = removeXmlAndComments($)
+  $ = removeStyles($)
+  return $
+}
+
+const stringify = ($) => {
+  return $.html()
+}
+
 const fixThisSucker = (junk) => {
   // parse as html
+  const parserOptions = {
+  }
+  const $ = cheerio.load(junk)
   // remove all the crap we don't want
+
+  const cleaned = removeCrap($)
+
   // stringify
-  return 'asdf'
+  return stringify($)
 }
 
 export default fixThisSucker
